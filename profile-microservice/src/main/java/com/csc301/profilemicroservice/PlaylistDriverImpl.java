f457f7d372dd067f8ab8831b137b05d1ad7054ba
Ryan Blasetti <ryan_blaset@pop-os.localdomain>
2020-11-28 20:04:05 -0500

    Path Added and Implemented: Created path to add songs to neo4j databse by id

--- profile-microservice/src/main/java/com/csc301/profilemicroservice/PlaylistDriverImpl.java
+++ profile-microservice/src/main/java/com/csc301/profilemicroservice/PlaylistDriverImpl.java
@@ -1,44 +1,61 @@
 package com.csc301.profilemicroservice;
 
+import java.util.HashMap;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.StatementResult;
-import org.springframework.stereotype.Repository;
 import org.neo4j.driver.v1.Transaction;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class PlaylistDriverImpl implements PlaylistDriver {
 
-	Driver driver = ProfileMicroserviceApplication.driver;
+  Driver driver = ProfileMicroserviceApplication.driver;
 
-	public static void InitPlaylistDb() {
-		String queryStr;
+  public static void InitPlaylistDb() {
+    String queryStr;
 
-		try (Session session = ProfileMicroserviceApplication.driver.session()) {
-			try (Transaction trans = session.beginTransaction()) {
-				queryStr = "CREATE CONSTRAINT ON (nPlaylist:playlist) ASSERT exists(nPlaylist.plName)";
-				trans.run(queryStr);
-				trans.success();
-			}
-			session.close();
-		}
-	}
+    try (Session session = ProfileMicroserviceApplication.driver.session()) {
+      try (Transaction trans = session.beginTransaction()) {
+        queryStr = "CREATE CONSTRAINT ON (nPlaylist:playlist) ASSERT exists(nPlaylist.plName)";
+        trans.run(queryStr);
+        trans.success();
+      }
+      session.close();
+    }
+  }
 
-	@Override
-	public DbQueryStatus likeSong(String userName, String songId) {
+  @Override
+  public DbQueryStatus likeSong(String userName, String songId) {
 
-		return null;
-	}
+    return null;
+  }
 
-	@Override
-	public DbQueryStatus unlikeSong(String userName, String songId) {
-		
-		return null;
-	}
+  @Override
+  public DbQueryStatus unlikeSong(String userName, String songId) {
 
-	@Override
-	public DbQueryStatus deleteSongFromDb(String songId) {
-		
-		return null;
-	}
+    return null;
+  }
+
+  @Override
+  public DbQueryStatus deleteSongFromDb(String songId) {
+
+    return null;
+  }
+
+  public DbQueryStatus addSongProfile(String songId) {
+    try (Session session = driver.session()) {
+      try (Transaction tx = session.beginTransaction()) {
+        String line = "MERGE (a:song {songId:$y})";
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("y", songId);
+        tx.run(line, params);
+        tx.success();
+      }
+      session.close();
+      return new DbQueryStatus("Add complete", DbQueryExecResult.QUERY_OK);
+    } catch (Exception e) {
+      return new DbQueryStatus("Add failed", DbQueryExecResult.QUERY_ERROR_GENERIC);
+    }
+  }
+
 }
